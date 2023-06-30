a40b66af9294f3801a62f8e57a3ec68b320bad8f
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-06-30 18:31:43 +0200

    refactor (routes):built in fieldsValidator register y login

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const authControllers = require('../controllers/auth');
 
-const {authorizer}= require('../middlewares')
+const {authorizer, fieldsValidator}= require('../middlewares')
+
+const signupValidator = fieldsValidator('email', 'username', 'password')
+const signinValidator = fieldsValidator('email', 'password')
+
 module.exports = (db) => {
-    router.post('/register',authControllers.signup(db))
-    router.post('/login',authControllers.signin(db))
+    router.post('/register', signupValidator,authControllers.signup(db))
+    router.post('/login', signinValidator,authControllers.signin(db))
     router.post('/signout',authorizer, authControllers.signout())
     return router
 }
