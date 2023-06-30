4766bcaec66d6969e87c43e34bf5471871ce4fc3
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-06-30 18:34:37 +0200

    refactor (index):users paths

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const authRouter = require('./auth')
-
+const usersRouter = require('./users')
 module.exports = (db) => {
     router.use('/auth',authRouter(db))
-
+    router.use('/users',usersRouter())
     return router
 }
