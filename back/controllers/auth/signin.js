515601d3c081fc412be7b1e9776841ff7c3d7d71
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-01 02:59:29 +0200

    refactor (signin.js): quitar console.log

--- back/controllers/auth/signin.js
+++ back/controllers/auth/signin.js
@@ -2,7 +2,7 @@ const error = require('../../misc/errors')
 const {selectUsers} = require('../../models/auth')
 const {hash, serialize} = require('simple-stateless-auth-library')
 module.exports = (db) => async(req, res, next)=>{
-    console.log(req)
+   
     const {username,email, password}= req.body
 
     if(!username|| !email || !password) return next(error[400])
