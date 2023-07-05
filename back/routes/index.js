46335a9389cf0e9eba50d485d281ca3980ce7213
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-05 02:35:54 +0200

    feat (routes):add route for events

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const authRouter = require('./auth')
 const usersRouter = require('./users')
 const eventoRouter = require('./eventos')
+const invitadosRouter = require ('./invitados')
 module.exports = (db) => {
     router.use('/auth',authRouter(db))
     router.use('/users',usersRouter())
     router.use('/evento',eventoRouter(db))
+    router.use('/invitados',invitadosRouter(db))
     return router
 }
