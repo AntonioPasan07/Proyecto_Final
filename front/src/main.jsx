2d0818365e275dfe4c93a2f754989fdae4c76846
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-07 19:39:13 +0200

    feat(main): preparing main.jsx fir the routes and leaving app.jsx in case  need it al the moment

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {Route,Redirect,Switch} from 'wouter'
+//import App from './App.jsx'
+const Container = ()=> {
+  return (
+    <h1>Hola Mundo</h1>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
 )
