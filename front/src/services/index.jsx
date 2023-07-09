be72cbf7208184046efcb25c7ddf1560210d4934
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-09 02:05:58 +0200

    refactor (services)

--- front/src/services/index.jsx
+++ front/src/services/index.jsx
@@ -1,7 +1,12 @@
-
+import {Info} from './user'
 import axios from 'axios'
 
 const client = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
+
+const user = {
+    Info: Info(client),
+}
+export {user}
