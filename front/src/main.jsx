19e74b57a16aef563de2055f85660a947836eea9
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-09 02:04:12 +0200

    refactor (main): routes for each page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Route,Redirect,Switch} from 'wouter'
 //import App from './App.jsx'
+import Provider from './context/Provider'
+import Inicio from './pages/Inicio'
+import Register from './pages/register'
+import Login from './pages/login'
+import Dashboard from './pages/dashboard'
+import CreateEvent from './pages/createEvent'
+import UnirseEvento from './pages/unirseEvento'
+import DetallesEvento from './pages/detallesEvento'
+import ListaInvitados from './pages/listaInvitados'
 const Container = ()=> {
   return (
+    <Provider>
     <h1>Hola Mundo</h1>
+    <Switch>
+    <Route path='/' component = {Inicio}/>
+    <Route path='/login'component = {Login}/>
+    <Route path='/register'component = {Register}/>
+    <Route path='/dashboard'component = {Dashboard}/>
+    <Route path='/createEvent'component = {CreateEvent}/>
+    <Route path='/unirseEvento'component = {UnirseEvento}/>
+    <Route path='/detalles'component = {DetallesEvento}/>
+    <Route path='/lista'component = {ListaInvitados}/>
+    </Switch>
+    </Provider>
   )
 }
 
