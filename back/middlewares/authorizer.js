64bc7c3cd02e779739352b51b1779330550206c2
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-10 21:07:57 +0200

    refactor(middleware):modified

--- back/middlewares/authorizer.js
+++ back/middlewares/authorizer.js
@@ -1,10 +1,11 @@
 const {deserialize}= require('simple-stateless-auth-library')
 const error = require('../misc/errors')
-module.exports = (req, res, next) => {
+module.exports =(strict = true) => (req, res, next) => {
     const payload = deserialize(req)
 
- if(!payload) return next(error[401])
+ if(strict && !payload) return next(error[401])
 
- res.locals = payload
+ res.locals.user = payload || {}
+ console.log(res.locals)
     next()
 }
