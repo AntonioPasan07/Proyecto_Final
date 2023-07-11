b51f4c6a351baacd8df7c52971de93ff9c2658d7
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-11 14:01:35 +0200

    feat (login): pages login missing functionality create components for all pages at the moment i want it to work

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,9 +1,39 @@
-
+//import { Link } from 'wouter';
+import {useLogin} from '../../hooks/useLogin'
+import { useForm } from 'react-hook-form';
+import { useAuth } from "../../hooks/useAuth";
+import { login } from '../../misc/templates';
 
 const Login = () => {
-    return (
-        <h1>Login</h1>
+  const isLoading = useLogin()
+    const { login: doLogin } = useAuth();
+  const { register, formState, handleSubmit } = useForm();
+  
 
-    )
-}
-export default Login
+  /*const handleFormSubmit = (data) => {
+    doLogin(data);
+  };*/
+  const { errors } = login;
+
+  return (
+    <section>
+      <h1>Login Page</h1>
+      {isLoading && <p>Loading...</p>}
+      <form onSubmit={handleSubmit(doLogin)}>
+        <label htmlFor="email">Email:</label><br />
+        <input id="email" placeholder="user@user.com" {...register("email", { required: true })} />
+        <p>{formState.errors && errors[formState.errors?.email?.type]}</p>
+
+        <label htmlFor="password">Password:</label><br />
+        <input id="password" type="password" placeholder="******" {...register("password", { required: true, minLength: 4 })} />
+        <p>{formState.errors && errors[formState.errors?.password?.type]}</p>
+    
+         
+            <input type="submit" />
+        
+      </form>
+    </section>
+  );
+};
+
+export default Login;
