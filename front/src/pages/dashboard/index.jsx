6fdbd33421e3ab638240d26179c20b1ef6b786ae
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-11 14:08:36 +0200

    feat (page): things need to be fixed because the data is not reading me correctly i only collect username

--- front/src/pages/dashboard/index.jsx
+++ front/src/pages/dashboard/index.jsx
@@ -1,7 +1,42 @@
+import { useLogin } from "../../hooks/useLogin"
+import { useUserEvent } from "../../hooks/useUserEvent"
 
+//import  { useState, useEffect } from "react"
+//import {useForm} from 'react-hook-form'
 const Dashboard = () => {
-    return (
-  <h1>Principal</h1>
-    )
+  const { data: user, isLoading } = useUserEvent()
+  console.log('data que trae>',user)
+  useLogin();
+  
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{user.data.username}</h2>
+      {user.data.length > 0 ? (
+        <ul>
+          {user.data.data.map((event) => (
+            <li key={event.eventId}>
+              <strong>Evento:</strong> {event.evento}
+              <br />
+              <strong>Fecha:</strong> {event.fecha_evento}
+              <br />
+              <strong>Hora:</strong> {event.hora}
+              <br />
+              <strong>Dirección:</strong> {event.direccion}
+              <br />
+              <strong>Descripción:</strong> {event.descripcion}
+              <br />
+              <br />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No events found.</p>
+      )}
+    </div>
+  )
 }
 export default Dashboard
