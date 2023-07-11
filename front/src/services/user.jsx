0af3214797d242b3f59768c94d01f45d6d9c7222
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-11 14:04:49 +0200

    refactor : services

--- front/src/services/user.jsx
+++
@@ -1,9 +0,0 @@
-export const Info = (client) => async()=> {
-    try{
-     const {data} = await client.get('/users')
-     console.info(data)
-     return data
-    }catch{error}{
-    return null
-    }
-   }
