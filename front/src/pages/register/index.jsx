0e1de72bc256d3fa780089680c655736b03fe90d
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-11 13:58:13 +0200

    feat (register): page register

--- front/src/pages/register/index.jsx
+++ front/src/pages/register/index.jsx
@@ -1,8 +1,41 @@
-
-
+import { useAuth } from "../../hooks/useAuth";
+import { useLogin } from "../../hooks/useLogin";
+import { useForm } from 'react-hook-form'
+import {login} from '../../misc/templates'
 const Register = () => {
+  const isLoading = useLogin()
+  const { register: registerAuth } = useAuth();
+  const { register,formState, handleSubmit } = useForm()
+  
+ /* const handleFormSubmit = (data) => {
+    registerAuth(data);
+  };*/
+  
+  const { errors } = login;
   return (
-    <h1>Register</h1>
+    <section>
+    <h1>Register Page</h1>
+    {isLoading && <p>Loading...</p>}
+    
+
+    <form onSubmit={handleSubmit(registerAuth)}>
+        <label htmlFor="email" email></label><br />
+        <input id="email" placeholder="user@user.com"{...register("email", { required: true })} />
+        <p>{formState.errors && errors[formState.errors?.email?.type]}</p>
+
+
+        <label htmlFor="username" username></label><br />
+        <input id="username" placeholder="username"{...register("username", { required: true })} />
+        <p>{formState.errors && errors[formState.errors?.username?.type]}</p>
+
+
+        <label htmlFor="password" password></label><br />
+        <input id="password" type="password" placeholder="******"{...register("password", { required: true, minLength: 4 })} />
+        <p>{formState.errors && errors[formState.errors?.password?.type]}</p>
+
+        <input type="submit" />
+    </form>
+</section>
   )
 }
 export default Register
