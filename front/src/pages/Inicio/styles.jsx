87d7a92211acb1a416a218173607fd4282f678ef
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 19:45:37 +0200

    feat (styles):I have to fix and modify styled

---
+++ front/src/pages/Inicio/styles.jsx
@@ -0,0 +1,18 @@
+
+
+import styled from "styled-components";
+
+
+ const Div = styled.div`
+ margin-top: 25px;
+ display: flex;
+ justify-content: space-around;
+`
+const Parrafo = styled.section`
+text-align: center;
+color: red;
+`
+
+export default Div;
+
+ 
