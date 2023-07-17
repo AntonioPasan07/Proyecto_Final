56cd9a27d1e4ef220e9d88dbc30bb74e1a00f7da
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 19:14:20 +0200

    feat(updete):invite put route controller for guests

--- back/controllers/invitaciones/update-guest-status.js
+++ back/controllers/invitaciones/update-guest-status.js
@@ -5,10 +5,10 @@ module.exports = (db)=> async (req, res, next) => {
 
     const {confirmacion, pagoBizum, estadoBizum, eventToken} = req.body
 
-    const {email}= res.locals
+    const {email}= res.locals.user
     
     const result = await updateGuests(await db)(confirmacion, pagoBizum, estadoBizum, eventToken, email)
-    
+    console.log(result)
    if (!result.ok) {
         return next(error[401]);
       }
