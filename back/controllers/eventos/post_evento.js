43fb9b143a3e33c1cefdd80b78a00aaba22fade7
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 19:11:44 +0200

    feat(post): post event handlers for user to imput data

--- back/controllers/eventos/post_evento.js
+++ back/controllers/eventos/post_evento.js
@@ -4,11 +4,11 @@ const error = require('../../misc/errors')
 
 module.exports = (db) => async (req, res, next)=> {
  const {evento, fechaEvento,hora,telefono, direccion, descripcion} =req.body  
-const {email}= res.locals
+const {email}= res.locals.user
     
     const result = await creatEvento(await db)(evento, fechaEvento,hora,telefono, direccion, descripcion, email)
    
-
+ 
     if(!result.ok)return next(error[400])
 
     res.status(200).json({
