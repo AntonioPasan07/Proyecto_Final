88163e5e78200f56667234883697f5b52191bd29
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 19:50:53 +0200

    clore(Title): componemts

---
+++ front/src/components/Title/index.jsx
@@ -0,0 +1,22 @@
+import { Titulo } from './styles';
+
+const Title = ({ texto }) => {
+  return <Titulo>{texto}</Titulo>;
+};
+
+export default Title;
+
+
+
+
+
+
+
+
+/*import Styled from './styles';
+
+const Title = () => {
+  return <Styled.Titulo>Gestiona tus eventos</Styled.Titulo>;
+};
+
+export default Title;*/
---
+++ front/src/components/Title/styles.jsx
@@ -0,0 +1,11 @@
+
+import styled from 'styled-components';
+
+const Titulo = styled.h1`
+font-size: 3rem;
+margin-top: 30px;
+text-align: center;
+margin-left: 6px;
+`;
+
+export { Titulo };
