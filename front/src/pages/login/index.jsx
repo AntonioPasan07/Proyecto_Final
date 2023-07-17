ff4526fab2a209723db8a20ac1046d02012f891e
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 20:29:31 +0200

    refactor (login):modified with components

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,23 +1,25 @@
-//import { Link } from 'wouter';
+
 import {useLogin} from '../../hooks/useLogin'
 import { useForm } from 'react-hook-form';
 import { useAuth } from "../../hooks/useAuth";
 import { login } from '../../misc/templates';
-
+import Title from '../../components/Title';
+import Imagen from '../../components/img'
 const Login = () => {
-  const isLoading = useLogin()
-    const { login: doLogin } = useAuth();
+  const isLoading = useLogin('/dashboard')
+  const { login: doLogin } = useAuth();
   const { register, formState, handleSubmit } = useForm();
   
 
-  /*const handleFormSubmit = (data) => {
-    doLogin(data);
-  };*/
+  
   const { errors } = login;
 
   return (
     <section>
-      <h1>Login Page</h1>
+      <div>
+        <Imagen src="https://www.rotularte.com.ar/wp-content/uploads/ideas-fiestas-eventos-empresariales.jpg" alt="Imagen de la página de Inicio" />
+      <Title texto = 'Login Page'/>
+      </div>
       {isLoading && <p>Loading...</p>}
       <form onSubmit={handleSubmit(doLogin)}>
         <label htmlFor="email">Email:</label><br />
@@ -27,8 +29,7 @@ const Login = () => {
         <label htmlFor="password">Password:</label><br />
         <input id="password" type="password" placeholder="******" {...register("password", { required: true, minLength: 4 })} />
         <p>{formState.errors && errors[formState.errors?.password?.type]}</p>
-    
-         
+        
             <input type="submit" />
         
       </form>
