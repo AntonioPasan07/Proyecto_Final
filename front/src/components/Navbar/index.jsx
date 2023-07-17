515fe9dcf75c4dba7f87be4b10b017c308669213
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-17 19:54:19 +0200

    feat (Navbar): Ihave to adjust it to implement it in the API

---
+++ front/src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">
+        <span role="img" aria-label="home">
+          🏠
+        </span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
