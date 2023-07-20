8ee1e6c6e6afeff99d5022afb992a718053714bb
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-20 22:13:02 +0200

    feat(Navbar):components

---
+++ front/src/components/Navbar/styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const NavbarContainer = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: cornflowerblue;
+  display: flex;
+  padding: 10px 20px;
+`;
+export {NavbarContainer};
