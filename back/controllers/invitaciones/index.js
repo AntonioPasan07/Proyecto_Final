6ed5f8ef57a05340777ed3de69e1c482414d4304
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-20 21:57:03 +0200

    feat(invitados):new get handler for guests

--- back/controllers/invitaciones/index.js
+++ back/controllers/invitaciones/index.js
@@ -1,7 +1,8 @@
 const postInvitados = require('./post-invitado')
 const updateGuestStatus = require('./update-guest-status')
-
+const getInvitados = require('./get-invitacin')
 module.exports ={
     postInvitados,
     updateGuestStatus,
+    getInvitados,
 }
