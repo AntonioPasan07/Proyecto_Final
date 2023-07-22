19df96a9ae3bf5f6162b0ab5a5044bfec79542bf
Antonio-Pasan07 <nanopas07@hotmail.com>
2023-07-22 09:50:56 +0200

    feat(styled): styles file for list page

---
+++ front/src/pages/listaInvitados/styles.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding: 20px;
+`;
+
+export const List = styled.ul`
+
+  list-style: none;
+  padding: 0;
+`;
+
+export const ListItem = styled.li`
+margin-left: 15px;
+color: white;
+margin-bottom: 10px;
+font-size: 18px;
+`;
+
+export const NoInvitadosMessage = styled.p`
+  font-size: 18px;
+  color: red;
+`;
